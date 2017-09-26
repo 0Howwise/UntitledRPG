67c87930481634434e7f3e8839a5b4e1f63dd5bc
0Howwise <Howwise82@gmail.com>
2017-09-26 09:36:09 -0700

    New Player Engine
    This commit adds a player engine that handles all and animations and player input. It can be expanded upon in the future.

--- UntitledRPG/Assets/Scripts/PlayerEngine.cs
+++ UntitledRPG/Assets/Scripts/PlayerEngine.cs
@@ -10,16 +10,18 @@ using UnityEngine;
 public class PlayerEngine : MonoBehaviour {
 
     //player movement variable
-    public float _Speed = 2;
+    public float _Speed = 4;
 
     /// <summary>
     /// This 
     /// </summary>
     [SerializeField]
     private Vector2 _DeltaForce;
+    private Vector2 _LastDirection;
     private Rigidbody2D _RGB;
     private Animator _Anim;
     private BoxCollider2D _BoxCollider;
+    private bool _IsMoving = false;
 
     /// <summary>
     /// initializing in a awake function in order to avoid crashing when changing variable when it isn't finished initializing yet
@@ -45,9 +47,21 @@ public class PlayerEngine : MonoBehaviour {
     /// This is where we read in the player's input.
     /// </summary>
     void CheckInput() {
+
+        _IsMoving = false;
+
         //storing built in unity inputs in variables
         var _H = Input.GetAxisRaw("Horizontal");
         var _V = Input.GetAxisRaw("Vertical");
+
+        //If h or v isn't equal to 0, than the player is moving 
+        if (_H < 0 || _H > 0 || _V < 0 || _V > 0)
+        {
+            _IsMoving = true;
+
+            if(!_BoxCollider.IsTouchingLayers(Physics2D.AllLayers))
+            _LastDirection = _RGB.velocity;
+        }
 
         _DeltaForce = new Vector2(_H, _V);
         CalculateMovement(_DeltaForce * _Speed);
@@ -63,5 +77,23 @@ public class PlayerEngine : MonoBehaviour {
         * button is being pressed */
         _RGB.velocity = Vector2.zero;
         _RGB.AddForce(_PlayerForce, ForceMode2D.Impulse);
+
+        SendAnimInfo();
+    }
+
+
+    /// <summary>
+    /// this is the function where you would send information to the qanimator
+    /// </summary>
+    void SendAnimInfo()
+    {
+        //what ever is the _RGB velocity . x will be read on to the animator same with y
+        _Anim.SetFloat("XSpeed", _RGB.velocity.x);
+        _Anim.SetFloat("YSpeed", _RGB.velocity.y);
+
+        _Anim.SetFloat("LastX", _LastDirection.x);
+        _Anim.SetFloat("LastY", _LastDirection.y);
+
+        _Anim.SetBool("IsMoving", _IsMoving);
     }
 }
